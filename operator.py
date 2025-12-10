94f7e1efbfda43977f789e6d77efa03f23712017
Saphal kafle <saphalkafle77@gmail.com>
2025-12-10 17:40:09 +0545

    logical operators

--- operator.py
+++ operator.py
@@ -17,3 +17,14 @@ print(a != b)
 # assignment operator
 a += b
 print(a)
+
+# logical operator
+# Not
+rain = False
+print(not (rain))
+
+print(not (5 > 3))
+
+# and
+print((5 > 3) and (5 > 1))
+print((5 > 2) or (6 > 8))
