886eb9b76a3a1a170b068c9d437e01633c79e6b9
Saphal kafle <saphalkafle77@gmail.com>
2025-12-10 10:05:43 +0545

    printing average

--- sumandaverage.py
+++ sumandaverage.py
@@ -1,10 +1,17 @@
 a = float(input("enter a:"))
 b = float(input("enter b:"))
+
 sum = a + b
+
 avg = (a + b) / 2
+
 difference = a - b
+
 multiple = a * b
+
 quotient = a / b
+
+print("average is :", avg)
 print("sum is ", sum)
 print("multiple is", multiple)
 print("difference is", difference)
