9422109a2043b2f2bc8a1c2c8bea9ba4550af282
Saphal kafle <saphalkafle77@gmail.com>
2025-12-11 19:43:50 +0545

    conversion of temperature

---
+++ temperature_conversion.py
@@ -0,0 +1,5 @@
+# conversion of celsius into fahrenheit
+temperature_celsius = str(input("enter temperature in celsius:"))
+temperature_celsius = float(temperature_celsius)
+fahrenheit_temp = (temperature_celsius * (9 / 5)) + 32
+print("conversion of celsius in fahrenheit is :", fahrenheit_temp)
