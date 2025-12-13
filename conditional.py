28ab70dabf440322d78af9090e475be15ba98680
Saphal kafle <saphalkafle77@gmail.com>
2025-12-13 20:31:21 +0545

    traffic lights

---
+++ conditional.py
@@ -0,0 +1,12 @@
+colour = input("enter colour:")
+if colour == "red":
+    print("stop")
+
+elif colour == "green":
+    print("go")
+
+elif colour == "yellow":
+    print("wait for red")
+
+else:
+    print("not a traffic light colour")
