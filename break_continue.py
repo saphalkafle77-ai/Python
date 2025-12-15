8525e37a18474062f1180666ec596a408e87b861
Saphal kafle <saphalkafle77@gmail.com>
2025-12-15 19:47:14 +0545

    break and continue loop

---
+++ break_continue.py
@@ -0,0 +1,18 @@
+# CONTINUE
+# i = 0
+# while i < 10:
+#     i += 1
+#     if i % 3 == 0:
+
+#         continue
+#     print(i)
+
+# print("outside of loop")
+
+# BREAK
+j = 1
+while j <= 10:
+    if j % 4 == 0:
+        break
+    print(j)
+    j += 1
