3e4574df12be5856fc15156ff6208e5bc9d6c9d2
Saphal kafle <saphalkafle77@gmail.com>
2025-12-25 20:53:56 +0545

    string in python

---
+++ conceptual/string.py
@@ -0,0 +1,14 @@
+# string slicing
+word = "saphal kafle"
+print(word[0 : len(word)])
+
+
+# string format
+# Normal formatting
+a = 4
+b = 3
+sum = a + b
+print("sum is {}".format(sum))
+
+# f string
+print(f"Sum of {a} and {b} is {a+b}")
