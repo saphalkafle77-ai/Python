7c324b1d7c79d764676cb2f3bff84479e01e91dc
Saphal kafle <saphalkafle77@gmail.com>
2025-12-26 20:16:40 +0545

    dictionary in python

---
+++ conceptual/dictionary.py
@@ -0,0 +1,17 @@
+# key.value pairs
+student = {"name": "saphal kafle", "Age": 20, "Address": "kathmandu"}
+print(student["name"])  # print info of name
+
+# mutable
+student["Age"] = 21
+print(student["Age"])
+
+# dictionary method
+print(student.keys())  # print keys
+print(student.values())  # print values
+print(student.items())  # print all items
+print(student.get("name"))  # find value it will print none if using get
+
+
+dict_student = list(student.values())
+print(dict_student)
