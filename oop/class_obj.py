96fa68f0b9d981ea9e86cfee16226428db7c8996
Saphal kafle <saphalkafle77@gmail.com>
2026-01-03 18:08:15 +0545

    basic about class and object

---
+++ oop/class_obj.py
@@ -0,0 +1,11 @@
+# mapping real world object with code
+# class = blueprint of object
+# object = instance of class(actual thing)
+class students:
+    subject = "Math"
+    college = "cab"
+
+
+stu1 = students()
+stu2 = students()
+print(stu1.subject, stu2.college)
